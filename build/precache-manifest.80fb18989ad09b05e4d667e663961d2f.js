febf825210a546a109dacbdfab95f296394de522
Le Dinh Luong <ledinhluong.std@gmail.com>
2020-10-03 09:50:33 +0700

    1

---
+++ build/precache-manifest.80fb18989ad09b05e4d667e663961d2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cff2fa1208a683078d95c118e33edb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4def29f8d89c4b28842",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9977479d82cb93b10b88",
+    "url": "/static/js/2.5b6a8167.chunk.js"
+  },
+  {
+    "revision": "ae255ce28eeaa07d30e5bc97a81b0fb7",
+    "url": "/static/js/2.5b6a8167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4def29f8d89c4b28842",
+    "url": "/static/js/main.9806ed79.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
